2741fcd3a2988983aa4d8832163e18c8ae0b4986
mrys7 <79912431+mrys7@users.noreply.github.com>
2021-04-26 10:34:02 +0200

    Add project files.

---
+++ ImageContrast/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace ImageContrast
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1103, 79);
+            this.panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(853, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Zapisz zdjęcie";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Załaduj zdjęcie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 79);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(288, 466);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(282, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Gray;
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.trackBar1);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(288, 79);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 466);
+            this.panel3.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(46, 116);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 28);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Kontrast Wariant 2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "delta_c";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(0, 19);
+            this.trackBar1.Maximum = 127;
+            this.trackBar1.Minimum = -128;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(200, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.TickFrequency = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(46, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 28);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Kontrast Wariant 1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.AutoScroll = true;
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel4.Controls.Add(this.chart1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(488, 79);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(615, 240);
+            this.panel4.TabIndex = 3;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "R";
+            series2.ChartArea = "ChartArea1";
+            series2.Color = System.Drawing.Color.Green;
+            series2.Legend = "Legend1";
+            series2.Name = "G";
+            series3.ChartArea = "ChartArea1";
+            series3.Color = System.Drawing.Color.Blue;
+            series3.Legend = "Legend1";
+            series3.Name = "B";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(612, 240);
+            this.chart1.TabIndex = 0;
+            this.chart1.TabStop = false;
+            this.chart1.Text = "chart1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(243, 212);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // panel5
+            // 
+            this.panel5.AutoScroll = true;
+            this.panel5.Controls.Add(this.pictureBox2);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(488, 319);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(615, 226);
+            this.panel5.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1103, 545);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ ImageContrast/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageContrast
+{
+    public partial class Form1 : Form
+    {
+        private int szer = 0, wys = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //załaduj zdjęcie
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+                szer = pictureBox1.Image.Width;
+                wys = pictureBox1.Image.Height;
+                pictureBox2.Image = new Bitmap(szer, wys);
+            }
+        }
+
+        //kontrast wariant 2
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap b1 = (Bitmap)pictureBox1.Image;
+            Bitmap b2 = (Bitmap)pictureBox2.Image;
+            Color k1, k2;
+            double delta_c = trackBar1.Value;
+            double red_p = 0;
+            double green_p = 0;
+            double blue_p = 0;
+
+            for (int x = 0; x < szer; x++)
+            {
+                for (int y = 0; y < wys; y++)
+                {
+                    k1 = b1.GetPixel(x, y);
+
+                    if (delta_c < 0)
+                    {
+                        if (k1.R < 127 + delta_c) red_p = ((127 / (127 + delta_c)) * (double)k1.R);
+                        else if (k1.R > 127 - delta_c) red_p = ((((127 * (double)k1.R) + (255 * delta_c)) / 127) + delta_c);
+                        else red_p = 127;
+
+                        if (k1.G < 127 + delta_c) green_p = ((127 / (127 + delta_c)) * (double)k1.G);
+                        else if (k1.G > 127 - delta_c) green_p = ((((127 * (double)k1.G) + (255 * delta_c)) / 127) + delta_c);
+                        else green_p = 127;
+
+                        if (k1.B < 127 + delta_c) blue_p = ((127 / (127 + delta_c)) * (double)k1.B);
+                        else if (k1.B > 127 - delta_c) blue_p = ((((127 * (double)k1.B) + (255 * delta_c)) / 127) + delta_c);
+                        else blue_p = 127;
+                    }
+                    else if (delta_c >= 0)
+                    {
+                        if (k1.R < 127) red_p = (((127 - delta_c) / 127) * (double)k1.R);
+                        if (k1.R >= 127) red_p = ((((127 - delta_c) / 127) * (double)k1.R) + (2 * delta_c));
+
+                        if (k1.G < 127) green_p = (((127 - delta_c) / 127) * (double)k1.G);
+                        if (k1.G >= 127) green_p = ((((127 - delta_c) / 127) * (double)k1.G) + (2 * delta_c));
+
+                        if (k1.B < 127) blue_p = (((127 - delta_c) / 127) * (double)k1.B);
+                        if (k1.B >= 127) blue_p = ((((127 - delta_c) / 127) * (double)k1.B) + (2 * delta_c));
+                    }
+                    if (red_p > 255) red_p = 255;
+                    if (green_p > 255) green_p = 255;
+                    if (blue_p > 255) blue_p = 255;
+                    if (red_p < 0) red_p = 0;
+                    if (green_p < 0) green_p = 0;
+                    if (blue_p < 0) blue_p = 0;
+                    k2 = Color.FromArgb((int)red_p, (int)green_p, (int)blue_p);
+                    b2.SetPixel(x, y, k2);
+                }
+            }
+            pictureBox2.Invalidate();
+
+            int[] r = new int[256];
+            int[] g = new int[256];
+            int[] b = new int[256];
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[2].Points.Clear();
+            for (int i = 0; i < b2.Height; i++)
+            {
+                for (int j = 0; j < b2.Width; j++)
+                {
+                    Color pixel = b2.GetPixel(j, i);
+                    r[pixel.R]++;
+                    g[pixel.G]++;
+                    b[pixel.B]++;
+                }
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                chart1.Series[0].Points.AddXY(i, r[i]);
+                chart1.Series[1].Points.AddXY(i, g[i]);
+                chart1.Series[2].Points.AddXY(i, b[i]);
+            }
+            chart1.Invalidate();
+        }
+
+
+        //zapisz zdjecie
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        pictureBox2.Image.Save(fs,
+                          System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+
+                    case 2:
+                        pictureBox2.Image.Save(fs,
+                          System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+
+                    case 3:
+                        pictureBox2.Image.Save(fs,
+                          System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+                }
+
+                fs.Close();
+            }
+        }
+
+        //konstrast wariant 1
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap b1 = (Bitmap)pictureBox1.Image;
+            Bitmap b2 = (Bitmap)pictureBox2.Image;
+            Color k1, k2;
+            double delta_c = trackBar1.Value;
+            double red_p = 0;
+            double green_p = 0;
+            double blue_p = 0;
+
+            for (int x = 0; x < szer; x++)
+            {
+                for (int y = 0; y < wys; y++)
+                {
+                    k1 = b1.GetPixel(x, y);
+                    if (delta_c < 0)
+                    {
+                        red_p = (((127 + delta_c) / 127) * (k1.R - delta_c));
+                        green_p = (((127 + delta_c) / 127) * (k1.G - delta_c));
+                        blue_p = (((127 + delta_c) / 127) * (k1.B - delta_c));
+                    }
+                    else if(delta_c >= 0)
+                    {
+                        red_p = ((127 / (127 - delta_c)) * (k1.R - delta_c));
+                        green_p = ((127 / (127 - delta_c)) * (k1.G - delta_c));
+                        blue_p = ((127 / (127 - delta_c)) * (k1.B - delta_c));
+                    }
+                    if (red_p > 255) red_p = 255;
+                    if (green_p > 255) green_p = 255;
+                    if (blue_p > 255) blue_p = 255;
+                    if (red_p < 0) red_p = 0;
+                    if (green_p < 0) green_p = 0;
+                    if (blue_p < 0) blue_p = 0;
+                    k2 = Color.FromArgb((int)red_p, (int)green_p, (int)blue_p);
+                    b2.SetPixel(x, y, k2);
+                }
+            }
+            pictureBox2.Invalidate();
+
+            int[] r = new int[256];
+            int[] g = new int[256];
+            int[] b = new int[256];
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[2].Points.Clear();
+            for (int i = 0; i < b2.Height; i++)
+            {
+                for (int j = 0; j < b2.Width; j++)
+                {
+                    Color pixel = b2.GetPixel(j, i);
+                    r[pixel.R]++;
+                    g[pixel.G]++;
+                    b[pixel.B]++;
+                }
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                chart1.Series[0].Points.AddXY(i, r[i]);
+                chart1.Series[1].Points.AddXY(i, g[i]);
+                chart1.Series[2].Points.AddXY(i, b[i]);
+            }
+            chart1.Invalidate();
+        }
+    }
+}
